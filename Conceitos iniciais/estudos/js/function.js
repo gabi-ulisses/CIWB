87cf424bffec6a307b824ea6ca871b69b017c051
gabi-ulisses <gabrielle.ulisses@aluno.ifsp.edu.br>
2024-10-18 20:12:31 -0300

    Exemplos

---
+++ Conceitos iniciais/estudos/js/function.js
@@ -0,0 +1,21 @@
+/*
+
+Forma 2:
+
+function processar(limite, statusCallback){
+    for(let i=0; i<limite; i++){
+        if(i % 10 == 0){
+            statusCallback(i);
+        }
+    }
+}
+
+processar(100, function(step){
+    console.log('Step:', step);
+});
+
+Forma 4:
+
+let somar = (a,b) => a + b;
+
+*/
---
+++ Conceitos iniciais/estudos/js/vetores.js
@@ -0,0 +1,18 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/*
+
+https://javascript.info/array-methods
+
+arr.filter(
+    (x) => x > 5
+);
+
+arr.map(
+    (x) => 2*x
+);
+
+arr.filter((x) => x > 7).map((x) => 2*x);
+
+*/
+
