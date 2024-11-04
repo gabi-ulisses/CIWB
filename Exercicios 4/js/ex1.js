a2c7e9b8b06514deedf766756891c823d6918a11
gabi-ulisses <gabrielle.ulisses@aluno.ifsp.edu.br>
2024-11-04 18:57:20 -0300

    Exercicios

---
+++ Exercicios 4/js/ex1.js
@@ -0,0 +1,24 @@
+function preencher() {
+    const data = {
+        "cep": "14802-000",
+        "logradouro": "Avenida Padre Francisco Sales Colturato",
+        "complemento": "lado par",
+        "unidade": "",
+        "bairro": "Centro",
+        "localidade": "Araraquara",
+        "uf": "SP",
+        "estado": "São Paulo",
+        "regiao": "Sudeste",
+        "ibge": "3503208",
+        "gia": "1818",
+        "ddd": "16",
+        "siafi": "6163"
+    };
+
+    var logradouro = document.getElementById("logradouro");
+    var bairro = document.getElementById("bairro");
+    var cidade = document.getElementById("cidade");
+    var estado = document.getElementById("estado");
+    
+}
+
---
+++ Exercicios 4/js/ex2.js
@@ -0,0 +1,12 @@
+function exercicio2() {
+    let produto2 = [
+        { 
+            id: 3,
+            nome: "Tinta para pincel para quadro branco",
+            preco: 12.90
+        }
+    ];
+
+    console.log(produto2);
+}
+
