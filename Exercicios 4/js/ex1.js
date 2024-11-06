456d401de981af3c92daf4424a510f190161b937
gabi-ulisses <gabrielle.ulisses@aluno.ifsp.edu.br>
2024-11-06 13:57:41 -0300

    New exercises

--- Exercicios 4/js/ex1.js
+++ Exercicios 4/js/ex1.js
@@ -1,24 +1,24 @@
-function preencher() {
-    const data = {
-        "cep": "14802-000",
-        "logradouro": "Avenida Padre Francisco Sales Colturato",
-        "complemento": "lado par",
-        "unidade": "",
-        "bairro": "Centro",
-        "localidade": "Araraquara",
-        "uf": "SP",
-        "estado": "São Paulo",
-        "regiao": "Sudeste",
-        "ibge": "3503208",
-        "gia": "1818",
-        "ddd": "16",
-        "siafi": "6163"
-    };
+const data = {
+    "cep": "14802-000",
+    "logradouro": "Avenida Padre Francisco Sales Colturato",
+    "complemento": "lado par",
+    "unidade": "",
+    "bairro": "Centro",
+    "localidade": "Araraquara",
+    "uf": "SP",
+    "estado": "São Paulo",
+    "regiao": "Sudeste",
+    "ibge": "3503208",
+    "gia": "1818",
+    "ddd": "16",
+    "siafi": "6163"
+};
 
-    var logradouro = document.getElementById("logradouro");
-    var bairro = document.getElementById("bairro");
-    var cidade = document.getElementById("cidade");
-    var estado = document.getElementById("estado");
-    
+function preencher() {
+    document.getElementById("logradouro").value = data.logradouro;
+    document.getElementById("bairro").value = data.bairro;
+    document.getElementById("cidade").value = data.localidade;
+    document.getElementById("estado").value = data.estado;
 }
 
+preencher();
--- Exercicios 4/js/ex2.js
+++ Exercicios 4/js/ex2.js
@@ -1,12 +1,33 @@
-function exercicio2() {
-    let produto2 = [
-        { 
-            id: 3,
-            nome: "Tinta para pincel para quadro branco",
-            preco: 12.90
-        }
-    ];
+const response = `[
+    {"id": 1, "nome": "Chai", "preco": 18.00},
+    {"id": 2, "nome": "Chang", "preco": 19.00},
+    {"id": 3, "nome": "Aniseed Syrup", "preco": 10.00},
+    {"id": 4, "nome": "Chef Anton's Cajun Seasoning", "preco": 22.00},
+    {"id": 5, "nome": "Chef Anton's Gumbo Mix", "preco": 21.35},
+    {"id": 6, "nome": "Grandma's Boysenberry Spread", "preco": 25.00},
+    {"id": 7, "nome": "Uncle Bob's Organic Dried Pears", "preco": 30.00},
+    {"id": 8, "nome": "Northwoods Cranberry Sauce", "preco": 40.00},
+    {"id": 9, "nome": "Mishi Kobe Niku", "preco": 97.00},
+    {"id": 10, "nome": "Ikura", "preco": 31.00}
+]`;
 
-    console.log(produto2);
+function preencherTabela() {
+    // Converte a string JSON em um array de objetos
+    const produtos = JSON.parse(response);
+    
+    // Seleciona o corpo da tabela usando getElementById
+    const tabelaBody = document.getElementById("tabela-produtos").getElementsByTagName("tbody")[0];
+
+    // Adiciona cada produto como uma nova linha na tabela
+    produtos.forEach(produto => {
+        const linha = `<tr>
+            <td>${produto.id}</td>
+            <td>${produto.nome}</td>
+            <td>${produto.preco.toFixed(2)}</td>
+        </tr>`;
+        tabelaBody.innerHTML += linha;
+    });
 }
 
+// Chama a função para preencher a tabela
+preencherTabela();
