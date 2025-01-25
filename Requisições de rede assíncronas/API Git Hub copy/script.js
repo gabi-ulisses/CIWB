be1415e2f21b17ca2e4b54d26f17d21887d1750d
gabi-ulisses <gabrielle.ulisses@aluno.ifsp.edu.br>
2025-01-25 11:01:33 -0300

    Add API OpenWeather

--- Requisições de rede assíncronas/API Git Hub copy/script.js
+++ Requisições de rede assíncronas/API Git Hub copy/script.js
@@ -1,82 +1,72 @@
-async function buscar(date) {
-    const url = `https://api.nasa.gov/planetary/apod?api_key=yax5hrTcj9uVgTX3NpWwoE2T3pQPFQ2b2OCZbvfv&date=${date}`;
-    let response = await fetch(url);
-
-    if (response.ok) {
-        let data = await response.json();
+async function buscarKosmos(date) {
+    const apiKeykosmos = 'yax5hrTcj9uVgTX3NpWwoE2T3pQPFQ2b2OCZbvfv';
+    const urlkosmos = `https://api.nasa.gov/planetary/apod?api_key=${apiKeykosmos}&date=${date}`;
+    
+    let responsekosmos = await fetch(urlkosmos);
+    
+    if (responsekosmos.ok) {
+        let datakosmos = await responsekosmos.json();
 
         let explicacao = document.querySelector('#explicacao');
-        explicacao.textContent = data['explanation'];
+        explicacao.textContent = datakosmos['explanation'];
 
         let title = document.querySelector('#title');
-        title.textContent = data['title'];
-        
+        title.textContent = datakosmos['title'];
+
         let img = document.querySelector('#image');
-        img.src = data['hdurl'];
+        img.src = datakosmos['hdurl'];
+        img.style.display = 'inline';
+
+        let dataImagem = document.querySelector('#dataImagem');
+        dataImagem.textContent = `Em: ${datakosmos['date']}`;
     } else {
-        alert('Erro ao buscar imagem');
+        alert('Erro ao buscar a imagem doKosmos. Tente novamente mais tarde.');
+    }
+}
+
+async function buscarClima(cidade) {
+    const apiKey = '9ca13acd02ac5b279c7c299f38de0cc9';
+    const urlClima = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&lang=pt_br&appid=${apiKey}`;
+    let responseClima = await fetch(urlClima);
+
+    if (responseClima.ok) {
+        let dataClima = await responseClima.json();
+
+        let climaDescricao = document.querySelector('#climaDescricao');
+        climaDescricao.textContent = dataClima.weather[0].description;
+
+        let climaTemperatura = document.querySelector('#climaTemperatura');
+        climaTemperatura.textContent = dataClma.main.temp + '°C';
+
+        let climaUmidade = document.querySelector('#climaUmidade');
+        climaUmidade.textContent = dataClima.main.humidity + '%';
+
+        let climaVento = document.querySelector('#climaVento');
+        climaVento.textContent = dataClima.wind.speed + ' km/h';
+
+        let climaIcone = document.querySelector('#climaIcone');
+        let iconCode = dataClima.weather[0].icon;
+        climaIcone.src = `http://openweathermap.org/img/wn/${iconCode}.png`;
+        climaIcone.style.display = 'inline';
+    } else {
+        alert('Erro ao buscar informações climáticas');
     }
 }
 
 function main() {
-    let btn = document.querySelector('#buscar');
-    btn.addEventListener('click', function() {
+    let btnkosmos = document.querySelector('#buscarKosmos');
+    btnkosmos.addEventListener('click', function() {
         let txtDate = document.querySelector('#date');
         let date = txtDate.value;
-        buscar(date);
+        buscarKosmos(date);
+    });
+
+    let btnClima = document.querySelector('#buscarClima');
+    btnClima.addEventListener('click', function() {
+        let txtCidade = document.querySelector('#cidade');
+        let cidade = txtCidade.value;
+        buscarClima(cidade);
     });
 }
 
 main();
-/*
-async function buscar(date) {
-    const url = `https://api.nasa.gov/planetary/apod?api_key=yax5hrTcj9uVgTX3NpWwoE2T3pQPFQ2b2OCZbvfv&date=${date}`;
-
-    let response = await fetch(url);
-
-    if (response.ok) {
-        let data = await response.json();
-
-        // Obter a explicação em inglês
-        let explicacaoEn = data['explanation'];
-
-        // Traduzir a explicação para o português usando LibreTranslate
-        let traducao = await traduzirParaPortugues(explicacaoEn);
-
-        let explicacao = document.querySelector('#explicacao');
-        explicacao.textContent = traducao;
-
-        let img = document.querySelector('#image');
-        img.src = data['hdurl'];
-    } else {
-        alert('Erro ao buscar imagem');
-    }
-
-}
-
-async function traduzirParaPortugues(texto) {
-    const res = await fetch("https://libretranslate.com/translate", {
-        method: "POST",
-        body: JSON.stringify({
-            q: texto,             // Texto a ser traduzido
-            source: "en",         // Idioma original (inglês)
-            target: "pt"          // Idioma de destino (português)
-        }),
-        headers: { "Content-Type": "application/json" }
-    });
-
-    const data = await res.json();
-    return data.translatedText;  // Retorna o texto traduzido
-}
-
-function main() {
-    let btn = document.querySelector('#buscar');
-    btn.addEventListener('click', function() {
-        let txtDate = document.querySelector('#date');
-        let date = txtDate.value;
-        buscar(date);
-    });
-}
-
-main();
-*/
